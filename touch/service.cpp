4e3a4e3ecf4810503edd66508e25e99dd2953b02
Giovanni Ricca <giovanniricca@proton.me>
2026-03-15 11:57:28 +0100

    hardware/nubia: touch: Implement HighTouchPollingRate
    Change-Id: Ife092eeb44a61ae3ae950dd15049d57080847d4c

---
+++ touch/service.cpp
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.touch-service.xiaomi"
+
+#include "HighTouchPollingRate.h"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+using aidl::vendor::lineage::touch::HighTouchPollingRate;
+
+int main() {
+    binder_status_t status = STATUS_OK;
+
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+
+    std::shared_ptr<HighTouchPollingRate> htpr = ndk::SharedRefBase::make<HighTouchPollingRate>();
+    const std::string htpr_instance = std::string(HighTouchPollingRate::descriptor) + "/default";
+    status = AServiceManager_addService(htpr->asBinder().get(), htpr_instance.c_str());
+    CHECK_EQ(status, STATUS_OK) << "Failed to add service " << htpr_instance << " " << status;
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}
